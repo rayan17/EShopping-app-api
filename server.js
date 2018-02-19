3aa08f4ff8d4d79b65cae84215dd0ab39aa96c43
Rayan Raza <rayan.raza93@gmail.com>
2018-02-19 23:53:10 +0500

    port changed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var categoriesController	= require('./controllers/CategoriesController');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-var port = process.env.port || 8090;
+var port = process.env.port || 3000;
 var router = express.Router();
 
 app.use(cors());
